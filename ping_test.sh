1e90c076e7403e1fe8cdda7e6944c5c8cda72070
joseph <etlars@gmail.com>
2018-08-24 19:47:49 +0900

    add creator

--- ping_test.sh
+++ ping_test.sh
@@ -1,6 +1,7 @@
 
 echo "-------------------------------------------"
 echo "kubectl  exec -it ubuntu -- ping 172.30.8.2"
+echo "      created by HyoJae                    "
 echo "-------------------------------------------"
 
 kubectl  exec -it ubuntu -- ping 172.30.8.2
